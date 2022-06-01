dfc9848649341d69299c433086fe591191338757
nebular-lab <hiranodaisuke0721@gmail.com>
2022-06-01 23:58:22 +0900

    アプリが動くようになった。100戦の取得、あるいは全戦の取得をすることが望ましい。

--- automate.py
+++ automate.py
@@ -7,9 +7,7 @@ class Selenium:
     element = None
 
     def __init__(self):
-        # https://selenium-python.readthedocs.io/api.html#module-selenium.webdriver.chrome.options
         options = webdriver.ChromeOptions()
-        # ブラウザを立ち上げないで、ブラウザ操作すること。
         options.add_argument("--headless")
         # https://daily.belltail.jp/?p=2691
         options.add_argument("--no-sandbox")
@@ -18,12 +16,8 @@ class Selenium:
         # https://developers.google.com/web/updates/2017/04/headless-chrome?hl=ja
         options.add_argument("--disable-gpu")
         # ウィンドウ幅設定
-        # options.add_argument("--window-size=1280x1696")
         options.add_argument("--window-size=1000x25000")
         # options.add_argument("--start-maximized")
-        # スクロールバーを表示しない
-        options.add_argument("--hide-scrollbars")
-        # ログ周り : https://qiita.com/grohiro/items/718239cdd36da42bd517#%E3%83%AD%E3%82%B0%E3%82%92%E5%87%BA%E3%81%99
         options.add_argument("--enable-logging")
         # ログ周り : https://qiita.com/grohiro/items/718239cdd36da42bd517#%E3%83%AD%E3%82%B0%E3%82%92%E5%87%BA%E3%81%99
         options.add_argument("--log-level=0")
@@ -32,10 +26,6 @@ class Selenium:
         # https://daily.belltail.jp/?p=2691
         options.add_argument("--disable-dev-shm-usage")
 
-        # LambdaのLayerを活用している。
-        # Layerとは、Lambda内に含まれるサービス名で、重いファイルや共通で利用するファイルを配置する場所。
-        # Lambdaとの紐付けを行うことで、紐づいたLambda関数ならば再利用可能。
-        # Lambda関数実行時には、/opt/以下にファイルが配置される。
         options.binary_location = "/opt/headless/python/bin/headless-chromium"
 
         self.driver = webdriver.Chrome(
@@ -51,7 +41,3 @@ class Selenium:
 
     def quit(self):
         self.driver.quit()
-
-    # # スクリーンショットを撮影する。
-    # def saveScreenshot(self, filePath):
-    #     self.driver.save_screenshot(filePath)
--- lambda_fanction.py
+++ lambda_fanction.py
@@ -1,104 +1,57 @@
 from selenium import webdriver
 import automate
-#import boto3
 import os
 
 # 環境変数HOMEを変更する。
-# もともとLambda自体、Linuxベースで動いている。 : https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/lambda-runtimes.html
-# 故に環境変数の扱いもLinuxを元にしていて、HOMEの環境変数は、「あなたのホームディレクトリのパス」を表す。
-# ホームディレクトリとは? : https://wa3.i-3-i.info/word11160.html
-# HOMEを/opt/に設定することでlambda_handler関数が実行される前に、/opt/以下のファイルを自動読みこみする。
-# /opt/以下に、.fontsを設置することで、ファイルの自動読み込みを行い、文字化け現象を防ぐ。
-# LambdaのLayerでアップロードしたコードは自動的に/opt/以下に配置される。
 os.environ['HOME'] = '/opt/'
 
-# s3のサービスを利用する。
-#s3 = boto3.resource('s3')
-
-# 撮影したいスクリーンショットURLを設定する。
-#URL = "https://www.yahoo.co.jp/"
-
-# スクリーンショットを一時的に格納するファイルパス名
-# /tmp以下に置く理由として、/tmp以外では、ファイルの読み書きができないため。
-#SCREEN_SHOT_PATH_NAME = "/tmp/screen_shot.png"
-
-# Bucketの名前
-#BUCKET_NAME = 'imglist'
-# S3へ書き出すファイルパス名
-#OUTPUT_FILE_NAME = 'screenshot.png'
-
-# Lambdaにおけるメイン関数
-# event : 最初の引数は イベントオブジェクト です。イベントは、処理する関数 Lambda のデータを含む JSON 形式のドキュメントです。 Lambda ランタイム は、イベントをオブジェクトに変換し、それを関数コードに渡します。
-# context : 2番目の引数は コンテキストオブジェクト です。コンテキストオブジェクトは、ランタイムに Lambda によって関数に渡されます。このオブジェクトは、呼び出し、関数、およびランタイム環境に関する情報を示すメソッドおよびプロパティを提供します。
-# https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/python-handler.html
-
-
 def lambda_handler(event, context):
-    # Seleniumに関するInstance生成を行う。
-    #selenium = automate.Selenium()
-
-    # スクリーンショットしたいページへアクセス
-    # selenium.access(URL)
-    # ページ読み込みのために遅延させる。
-    # selenium.stop(5)
-
-    # スクリーンショットを実行して、一時的にファイル保存する。
-    # selenium.saveScreenshot(SCREEN_SHOT_PATH_NAME)
-
-    # スクリーンショットをS3にアップロードする。
-    # s3.meta.client.upload_file(
-    #     SCREEN_SHOT_PATH_NAME, BUCKET_NAME, OUTPUT_FILE_NAME)
-
-    # Selenium処理終了
-    # selenium.quit()
-
-    selenium = automate.Selenium()
-    url = 'https://nodocchi.moe/tenhoulog/#!&name=%E3%81%AD%E3%81%B6%E3%81%A4%E3%83%BC'
-
+    name=event['playerName']
+    
+    selenium=automate.Selenium()
+    url='https://nodocchi.moe/tenhoulog/#!&name='+name
+    
     selenium.access(url)
-    selenium.stop(1)
-
+    selenium.stop(7)
+    
     rule = selenium.driver.find_element_by_id('f_sct__SCType_enabled')
-    selenium.stop(0.5)
+    selenium.stop(1)
     rule.click()
-
+    
     # ルールの設定
     # (span[5]なら鳳凰)
     tokujou = selenium.driver.find_element_by_xpath(
         '//*[@id="div_filter"]/div[2]/form/fieldset/div[3]/span[4]/label')
-    selenium.stop(0.5)
+    selenium.stop(1)
     tokujou.click()
-
+    
     # 四麻ボタン
     yonma = selenium.driver.find_element_by_xpath(
         '//*[@id="div_filter"]/div[2]/form/fieldset/div[2]/span[3]/label')
-    selenium.stop(0.5)
+    selenium.stop(1)
     yonma.click()
-
+    
     # 適用ボタン
     tekiyou = selenium.driver.find_element_by_xpath(
         '//*[@id="div_filter"]/div[2]/form/fieldset/legend/input[2]')
-    selenium.stop(0.5)
+    selenium.stop(1)
     tekiyou.click()
+    
+    
+    #一番下のindexを取得して、100戦分を指定するべき
+    #レート
+    xpath='//*[@id="tbl_list"]/tbody/tr[position()>1500]/td[6]'
+    
+    # xpath = '//*[@id="tbl_list"]/tbody/tr[position()>683]/td[5]/span/sub'
 
-    xpath = '//*[@id="tbl_list"]/tbody/tr[position()>683]/td[5]/span/sub'
+    selenium.stop(10)
 
-    # スクロール
-    count = 1
-    for i in range(count):
-        selenium.driver.execute_script(
-            "window.scrollTo(0,"+str(i*3000)+")")
-        selenium.stop(10)
     elems = selenium.driver.find_elements_by_xpath(xpath)
 
     scores = []
     for elem in elems:
-        scores.append(elem.text.replace('pt', ''))
+        scores.append(int(elem.text.replace('R', '')))
+    return {
+        "result": scores
+    }
 
-    scores_int = []
-    for score in scores:
-        scores_int.append(int(score))
-
-    selenium.quit()
-
-    return scores_int
